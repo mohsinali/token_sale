6dec2f9fb426b466a91ef04f5be13ecdd2fdecd5
Mohsin <mohammadmohsin@gmail.com>
2018-10-08 01:07:49 +0500

    Token Sale smart contract initialization

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
-var DappToken = artifacts.require("./DappToken.sol");
+var DappToken     = artifacts.require("./DappToken.sol");
+var DappTokenSale = artifacts.require("./DappTokenSale.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(DappToken, 1000000);
+  deployer.deploy(DappToken, 1000000).then(function(){
+    var tokenPrice = 1000000000000000; // Token Price is 0.001 ether
+    return deployer.deploy(DappTokenSale, DappToken.address, tokenPrice);
+  });
 };
